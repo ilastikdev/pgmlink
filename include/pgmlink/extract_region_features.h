6a99834a8d495222cc14b515025ac8c81c950a3a
Carsten Haubold <carstenhaubold@googlemail.com>
2014-11-03 10:56:40 +0100

    Add region feature computation for ROIs

--- include/pgmlink/extract_region_features.h
+++ include/pgmlink/extract_region_features.h
@@ -6,6 +6,7 @@
 #include "pgmlink/featurestore.h"
 #include "pgmlink/log.h"
 #include <iostream>
+#include <numeric>
 
 namespace pgmlink {
 namespace features{
@@ -34,6 +35,19 @@ void set_feature(FeatureMap &feature_map, const std::string &name, vigra::TinyVe
     feature_map[name].push_back(feature_type(value[2]));
 }
 
+template<int N, typename T1, typename T2>
+void set_feature_with_offset(FeatureMap &feature_map,
+                 const std::string &name,
+                 vigra::TinyVector<T1,N> value,
+                 vigra::TinyVector<T2,N> offset)
+{
+    feature_map[name].clear();
+    for(int i = 0; i < N; i++)
+    {
+        feature_map[name].push_back(feature_type(value[i] + offset[i]));
+    }
+}
+
 template<>
 void set_feature(FeatureMap &feature_map, const std::string &name, vigra::linalg::Matrix<double> value)
 {
@@ -45,14 +59,20 @@ void set_feature(FeatureMap &feature_map, const std::string &name, vigra::linalg
 }
 
 ///
-/// Extract features from all regions in the given MultiArrayView and insert them into the corresponding traxels at the current timestep.
+/// Extract features from the selected regions in the given MultiArrayView
+/// and insert them into the corresponding traxels (with offset) at the current timestep.
+/// Coordinates are shifted by the given offset if this is only a ROI of the full image.
 /// \return the maximal label id
 ///
 template<int N, typename DataType, typename LabelType>
-int extract_region_features(const vigra::MultiArrayView<N, DataType> data,
-                             const vigra::MultiArrayView<N, LabelType> labels,
-                             boost::shared_ptr<pgmlink::FeatureStore> fs,
-                             unsigned int timestep)
+void extract_region_features_roi(const vigra::MultiArrayView<N, DataType>& data,
+                                 const vigra::MultiArrayView<N, LabelType>& labels,
+                                 const std::vector<size_t>& label_indices,
+                                 unsigned int traxel_index_offset,
+                                 const vigra::TinyVector<size_t, N>& coord_offsets,
+                                 boost::shared_ptr<pgmlink::FeatureStore> fs,
+                                 unsigned int timestep,
+                                 bool verbose = true)
 {
     // extract features using vigra
     using namespace vigra::acc;
@@ -86,17 +106,14 @@ int extract_region_features(const vigra::MultiArrayView<N, DataType> data,
     extractFeatures(data, labels, a);
     LOG(pgmlink::logDEBUG1) << "Finished feature extraction for frame " << timestep;
 
-    LabelType label_min, label_max;
-    labels.minmax(&label_min, &label_max);
-
     // insert features into featurestore
-    for(size_t label = label_min; label < label_max; label++)
+    for(LabelType label : label_indices)
     {
         if(label == 0) // ignore background
             continue;
 
         // get respective feature map from FeatureStore
-        pgmlink::FeatureMap& feature_map = fs->get_traxel_features(timestep, label);
+        pgmlink::FeatureMap& feature_map = fs->get_traxel_features(timestep, label + traxel_index_offset);
 
         // add features
         set_feature(feature_map, "Mean", get<Mean>(a, label));
@@ -104,9 +121,9 @@ int extract_region_features(const vigra::MultiArrayView<N, DataType> data,
         set_feature(feature_map, "Variance", get<Variance>(a, label));
         set_feature(feature_map, "Count", get<Count>(a, label));
         set_feature(feature_map, "RegionRadii", get<RegionRadii>(a, label));
-        set_feature(feature_map, "RegionCenter", get<RegionCenter>(a, label));
-        set_feature(feature_map, "Coord< Maximum >", get<Coord< Maximum > >(a, label));
-        set_feature(feature_map, "Coord< Minimum >", get<Coord< Minimum > >(a, label));
+        set_feature_with_offset(feature_map, "RegionCenter", get<RegionCenter>(a, label), coord_offsets);
+        set_feature_with_offset(feature_map, "Coord< Maximum >", get<Coord< Maximum > >(a, label), coord_offsets);
+        set_feature_with_offset(feature_map, "Coord< Minimum >", get<Coord< Minimum > >(a, label), coord_offsets);
         set_feature(feature_map, "RegionAxes", get<RegionAxes>(a, label));
         set_feature(feature_map, "Kurtosis", get<Kurtosis>(a, label));
         set_feature(feature_map, "Minimum", get<Minimum>(a, label));
@@ -117,7 +134,29 @@ int extract_region_features(const vigra::MultiArrayView<N, DataType> data,
         set_feature(feature_map, "Central< PowerSum<4> >", get<Central< PowerSum<4> > >(a, label));
         set_feature(feature_map, "Weighted<PowerSum<0> >", get<Weighted<PowerSum<0> > >(a, label));
     }
+}
 
+///
+/// Extract features from all regions in the given MultiArrayView
+/// and insert them into the corresponding traxels at the current timestep.
+/// \return the maximal label id
+///
+template<int N, typename DataType, typename LabelType>
+int extract_region_features(const vigra::MultiArrayView<N, DataType>& data,
+                             const vigra::MultiArrayView<N, LabelType>& labels,
+                             boost::shared_ptr<pgmlink::FeatureStore> fs,
+                             unsigned int timestep)
+{
+    // create list of all labels we want to extract features for
+    LabelType label_min, label_max;
+    labels.minmax(&label_min, &label_max);
+    std::vector<size_t> label_indices(label_max - label_min);
+    std::iota(label_indices.begin(), label_indices.end(), label_min);
+
+    // coordinate offset is zero, use default constructor
+    vigra::TinyVector<size_t, N> coord_offset;
+
+    extract_region_features_roi<N, DataType, LabelType>(data, labels, label_indices, 0, coord_offset, fs, timestep, false);
     return label_max;
 }
 
--- python/pyextract_region_features.cpp
+++ python/pyextract_region_features.cpp
@@ -20,6 +20,37 @@ int py_extract_region_features(
     return pgmlink::features::extract_region_features<N, DataType, LabelType>(image, labels, fs, timestep);
 }
 
+template<int N, typename DataType, typename LabelType>
+void py_extract_region_features_roi(
+        const vigra::NumpyArray<N, DataType>& image,
+        const vigra::NumpyArray<N, LabelType>& labels,
+        const vigra::NumpyArray<1, vigra::Int64>& label_indices,
+        int traxel_index_offset,
+        const vigra::NumpyArray<1, vigra::Int64>& coord_offsets,
+        boost::shared_ptr<pgmlink::FeatureStore> fs,
+        const size_t timestep
+        )
+{
+    vigra::TinyVector<size_t, N> coord_offsets_tv;
+    for (size_t idx = 0; idx < N; ++idx) {
+      coord_offsets_tv[idx] = coord_offsets[idx];
+    }
+
+    std::vector<size_t> label_indices_std;
+    for (auto it = label_indices.begin(); it != label_indices.end(); ++it)
+    {
+        label_indices_std.push_back(*it);
+    }
+
+    pgmlink::features::extract_region_features_roi<N, DataType, LabelType>(image,
+                                                                           labels,
+                                                                           label_indices_std,
+                                                                           (unsigned int)traxel_index_offset,
+                                                                           coord_offsets_tv,
+                                                                           fs,
+                                                                           (unsigned int)timestep);
+}
+
 void export_region_feature_extraction()
 {
     using namespace boost::python;
@@ -27,4 +58,7 @@ void export_region_feature_extraction()
     // uint32
     def("extract_region_features", vigra::registerConverters(&py_extract_region_features<3, float, vigra::UInt32>));
     def("extract_region_features", vigra::registerConverters(&py_extract_region_features<2, float, vigra::UInt32>));
+
+    def("extract_region_features_roi", vigra::registerConverters(&py_extract_region_features_roi<3, float, vigra::UInt32>));
+    def("extract_region_features_roi", vigra::registerConverters(&py_extract_region_features_roi<2, float, vigra::UInt32>));
 }
